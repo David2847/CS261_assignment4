bad73dd5c40a728c409c37a16e4c1c95ed98cfe3
David Jantz <dljantz@gmail.com>
2024-11-18 17:25:06 -0500

    Finished!

--- avl.py
+++ avl.py
@@ -110,7 +110,6 @@ class AVL(BST):
         while curr_node is not None:
             self._rebalance(curr_node)  # rebalance as necessary
             curr_node = curr_node.parent
-        x = 1
 
     def _recursive_add(self, value: object, curr_node: AVLNode) -> AVLNode | None:
         """
@@ -147,27 +146,123 @@ class AVL(BST):
 
     def remove(self, value: object) -> bool:
         """
-        TODO: Write your implementation
+        Removes value from a tree, restructures the tree to preserve BST qualities, rebalances the tree, 
+        and returns true or false to communicate whether removal was successful.
         """
-        # remove node using normal BST removal (replace with inorder successor)
-        #if super().remove(value):
-        pass
-            # update heights
-            # rebalance
+        # traverse down the tree until finding the node to remove
+        nodes = self.get_node_and_parent(value)
+        parent = nodes[0]
+        child = nodes[1]
 
-    # Experiment and see if you can use the optional                         #
-    # subtree removal methods defined in the BST here in the AVL.            #
-    # Call normally using self -> self._remove_no_subtrees(parent, node)     #
-    # You need to override the _remove_two_subtrees() method in any case.    #
-    # Remove these comments.                                                 #
-    # Remove these method stubs if you decide not to use them.               #
-    # Change this method in any way you'd like.                              #
+        # edge case: child doesn't exist (is None). removal failed, return False
+        if child is None:
+            return False
+
+        has_left_subtree = child.left is not None
+        has_right_subtree = child.right is not None
+
+        # handle the four cases -- it has zero, left, right, or both subtrees.
+        if has_left_subtree and has_right_subtree:
+            curr_node = self._remove_two_subtrees(parent, child)
+        elif has_left_subtree and not has_right_subtree:
+            self._remove_one_subtree(parent, child)
+            curr_node = parent
+        elif not has_left_subtree and has_right_subtree:
+            self._remove_one_subtree(parent, child)
+            curr_node = parent
+        else:
+            self._remove_no_subtrees(parent, child)
+            curr_node = parent
+
+        # REBALANCING:
+        # if removed node had no subtrees, start at its parent and crawl back up to the root,
+        #       rebalancing and updating heights as you go
+        # if removed node had one subtree, do the same thing
+        # if removed node had two subtrees, have to go back to inorder successor or its parent and work up from there
+
+        # while loop -- work up the tree, continually jumping to next parent for rebalancing and height updating
+        while curr_node is not None:
+            self._rebalance(curr_node)
+            curr_node = curr_node.parent
+
+        return True
+
+    def _remove_one_subtree(self, remove_parent: AVLNode, remove_node: AVLNode) -> None:
+        """
+        Removes a node from the tree that has one subtree.
+        :param remove_parent: the parent of the node to be removed
+        :param remove_node: the node to be removed
+        :return: None
+        """
+        # edge case -- remove_node is root. Promote its left or right child to become the new root
+        if remove_parent is None:
+            if remove_node.left is not None:
+                self._root = remove_node.left
+            else:
+                self._root = remove_node.right
+            self._root.parent = None
+            return
+
+        # check to see if remove node is left or right and act accordingly... four cases:
+        #       remove left child of parent, promote left grandchild
+        #       remove left child of parent, promote right grandchild
+        #       remove right child of parent, promote left grandchild
+        #       remove right child of parent, promote right grandchild
+        if remove_parent.left == remove_node:
+            if remove_node.left is not None:
+                remove_parent.left = remove_node.left
+                remove_node.left.parent = remove_parent
+            else:
+                remove_parent.left = remove_node.right
+                remove_node.right.parent = remove_parent
+        else:
+            if remove_node.left is not None:
+                remove_parent.right = remove_node.left
+                remove_node.left.parent = remove_parent
+            else:
+                remove_parent.right = remove_node.right
+                remove_node.right.parent = remove_parent
 
     def _remove_two_subtrees(self, remove_parent: AVLNode, remove_node: AVLNode) -> AVLNode:
         """
-        TODO: Write your implementation
+        Removes a node from the AVL tree that has two subtrees of its own.
+        :param remove_parent: parent of the node to be removed
+        :param remove_node: node to be removed
+        :return: the former parent of the inorder successor, so that rebalancing has the appropriate starting point
         """
-        pass
+        inorder_parent, inorder_successor = self.get_inorder_successor_and_parent(remove_node)
+
+        # if the inorder successor is not the immediate right child of remove_node, we have to:
+        #       make S's right child PS's left child
+        #       make N's right child S's right child
+        if inorder_parent != remove_node:
+            inorder_parent.left = inorder_successor.right
+            if inorder_successor.right is not None:
+                inorder_successor.right.parent = inorder_parent
+            inorder_successor.right = remove_node.right
+            remove_node.right.parent = inorder_successor
+
+        # Regardless of which descendant the inorder successor is, we need to:
+        #       make N's left child S's left child
+        #       make S become PN's left or right child, or make S into self._root
+        inorder_successor.left = remove_node.left
+        remove_node.left.parent = inorder_successor
+        if remove_parent is None:
+            self._root = inorder_successor
+            inorder_successor.parent = None
+        elif remove_parent.left == remove_node:
+            remove_parent.left = inorder_successor
+            inorder_successor.parent = remove_parent
+        else:
+            remove_parent.right = inorder_successor
+            inorder_successor.parent = remove_parent
+
+        # it's important to return the proper reference here... usually inorder_parent,
+        #       but if the inorder successor was the direct right child of the node being
+        #       removed, we start rebalancing at the inorder successor itself.
+        if inorder_parent == remove_node:
+            return inorder_successor
+        return inorder_parent
 
     def _balance_factor(self, node: AVLNode) -> int:
         """
@@ -279,7 +374,7 @@ class AVL(BST):
             if self._balance_factor(node.left) > 0:
                 # rotate left child to the left (handles L-R imbalance)
                 self._rotate_left(node.left)
-                self._update_height(node.left.left)
+                self._update_height(node.left.left)  # rebalance in between rotations
             # rotate N right (handles L-L imbalance)
             self._rotate_right(node)
         # if unbalanced right:
@@ -288,7 +383,7 @@ class AVL(BST):
             if self._balance_factor(node.right) < 0:
                 # rotate right child to the right (handles R-L imbalance)
                 self._rotate_right(node.right)
-                self._update_height(node.right.right)
+                self._update_height(node.right.right) # rebalance in between rotations
             # rotate N left (handles R-R imbalance)
             self._rotate_left(node)
         self._update_height(node)
